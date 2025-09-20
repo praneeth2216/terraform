1a53151ed99f24330f8e72174e09949a1b1fede2
praneeth2216 <praneeth2216@hotmail.com>
2025-09-20 17:36:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,52 @@
 terraform {
   required_providers {
-    aws = { 
-       source = "hashicorp/aws"
-       version = "~> 3.0" 
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
     }
-  } 
+  }
 
- backend "s3" {
-  bucket = "tf-state-terraform"
-  key = "state/terraform.tfstate"
-  dynamodb_table="terraform-locks"
-  encrypt = true  
- } 
+  backend "s3" {
+    bucket = "karupothula1"
+    key = "state/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table="terraform-locks"
+    encrypt = true
+  }
+}
 
-}   
 
 provider "aws" {
   region = "us-east-2"
-} 
+}
 
-resource "aws_instance" "devops" {
-   ami = "ami-09786574678"
-   instance_type = "t3.micro"
-   key_name = "terraform"
+resource "aws_instance" "powershell" {
+  ami = "ami-0ca4d5db4872d0c28"
+  instance_type = "t3.micro"
+  key_name = "terraform"
 
-   tags = {
-    Name = "devops"
-    Environment = "dev"
-    } 
-} 
+  tags = {
+    Name = "powershell"
+  }
+}
+
+resource "aws_instance" "shell" {
+  ami = "ami-0ca4d5db4872d0c28"
+  instance_type = "t3.micro"
+  key_name = "terraform"
+  tags = {
+    Name = "shell"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "karupothula2" 
+  acl    = "private"                      
+  versioning {
+    enable = true
+  }
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
